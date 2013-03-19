2b9f9cf63eace555962cb08c003a0b498065bd11
vyshnevska <vyshnevska.n@gmail.com>
2013-03-19 23:53:51 +0200

    Added validations for game deyail.

--- app/models/game_detail.rb
+++ app/models/game_detail.rb
@@ -4,4 +4,7 @@ class GameDetail < ActiveRecord::Base
   belongs_to :question
   belongs_to :answer
 
+  validates :game_id, :presence => { :message => "game must be assigned" }
+  validates :question_id, :presence => { :message => "question must be assigned" }
+  validates :answer_id, :presence => { :message => "answer must be assigned" }
 end

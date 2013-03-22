03fcba3afbc2ffe7ec65e1a14ed47a6a4f1f94e4
vyshnevska <vyshnevska.n@gmail.com>
2013-03-22 14:22:32 +0200

    precompile assets.

--- db/schema.rb
+++ db/schema.rb
@@ -66,11 +66,7 @@ ActiveRecord::Schema.define(:version => 20130322093418) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
-    t.string   "role"
     t.boolean  "admin",                  :default => false, :null => false
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end

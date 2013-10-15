e1a58cbb4cac7eb75b648e7df638fe468b92eb01
Nina Vyshnevska <vyshnevska.n@gmail.com>
2013-10-16 00:31:29 +0300

    Repair Quizz creation.

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -2,5 +2,24 @@ jQuery(function() {
 	$(".alert .close").click(function(){
 		$(".alert-success").hide();
 	});
-	
+
+	$("#add_quizz").click(function(){
+		var title = $("#quizz_title").val(),
+			errors = $(".errors");
+		if (title != ""){
+			errors.hide();
+			$("#questions").show();
+			$("#answers").show();
+			//$("#add_quizz").attr("disable", true);//Do not work properly
+			//$("#submit").attr("disable", false);
+		}else{
+			errors.html("Quizz Title can't be blank!");
+			errors.show();
+		}
+	});
+
+	// $("#submit").click(function(){
+	// 	// var vid_id = document.getElementById("vid_vid").innerHTML;
+ //   		$.post("/quizzs/create");
+	// });
 });
--- app/helpers/quizzs_helper.rb
+++ app/helpers/quizzs_helper.rb
@@ -1,5 +1,5 @@
 module QuizzsHelper
 	def page_title(name, action = nil)
-		action.nil? ? content_tag("h1", name) : content_tag("h3", name)
+		action.nil? ? content_tag("h2", name) : content_tag("h5", name)
 	end
 end

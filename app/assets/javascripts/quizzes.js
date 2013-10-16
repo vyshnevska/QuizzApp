504a06793e686393aa063e5c3d9e01f4db4ebf16
Nina Vyshnevska <vyshnevska.n@gmail.com>
2013-10-17 00:51:58 +0300

    Added js for creating Quizz.

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -1,25 +1,70 @@
 jQuery(function() {
-	$(".alert .close").click(function(){
+	$(".alert .close").click(function() {
 		$(".alert-success").hide();
 	});
 
-	$("#add_quizz").click(function(){
+	enableQuizzBtn();
+	$("#expand").css("opacity", "0.5");
+
+	//Temporary
+	$(".questions").show();
+	$(".answers").show();
+
+	$("#quizz_title").focusout(function() {
+		enableQuizzBtn();	
+	});
+	function enableQuizzBtn() {
+		if($("#quizz_title").val() != "") {
+			// $("#add_quizz").removeAttr("disabled");
+			$("#submit").removeAttr("disabled");
+		} else {
+			$("#submit").attr("disabled", "disabled");
+		}
+		validateQuizz();
+	};
+
+	function validateQuizz() {
 		var title = $("#quizz_title").val(),
 			errors = $(".errors");
-		if (title != ""){
+		if(title != "") {
 			errors.hide();
-			$("#questions").show();
-			$("#answers").show();
-			//$("#add_quizz").attr("disable", true);//Do not work properly
-			//$("#submit").attr("disable", false);
-		}else{
+			$(".questions").show();
+			$(".answers").show();
+			// $("#add_quizz").attr("disabled", "disabled");//Do not work properly
+		} else {
 			errors.html("Quizz Title can't be blank!");
 			errors.show();
 		}
+	};
+
+	$("#collapse").click(function() {
+		$(".answers").hide();
+		$(".questions").addClass("collapsed");
+		$("#expand").attr("display", "inline-block");
+		$("#collapse").css("opacity", "0.5");
+		$("#expand").css("opacity", "1");
 	});
 
-	// $("#submit").click(function(){
-	// 	// var vid_id = document.getElementById("vid_vid").innerHTML;
- //   		$.post("/quizzs/create");
-	// });
+	$("#expand").click(function() {
+		$(".answers").show();
+		$(".questions").addClass("expanded");
+		$("#collapse").attr("display", "inline-block");
+		$("#expand").css("opacity", "0.5");
+		$("#collapse").css("opacity", "1");
+	});
+
+	$("#add_answer").click(function(){
+		var id = $(".answers input").length +1;
+		$answer = $('<div class="line" id =' + id +'><input id="answers_content" type="text" value="answ77" style="margin-left: 73px;" name="answers[content]"/><img src="/assets/cancel.png" alt="Delete Answer" class="remove_answer"></div>');
+		$('.answers').append($answer);
+	});
+
+	$('.line').on('click', ".remove_answer", function(){
+		$(this).parent().empty();
+	});
+
+	$("#add_qst").click(function(){
+		//TODO: implement this
+	});
+
 });

fcd0a3ca42f5ff844f0313ed331eb42ea6ef673f
Nina Vyshnevska <vyshnevska.n@gmail.com>
2013-10-23 21:14:17 +0300

    Facebook authentication added.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+   devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :role
+  attr_accessible :provider, :uid, :name
 
   has_many :games
   has_many :quizzs, :through => :games
@@ -20,17 +21,17 @@ class User < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
   after_create :assign_role
 
-  def self.create_with_omniauth(auth)
-    binding.pry
-    create! do |user|
-      user.provider = auth['provider']
-      user.uid = auth['uid']
-      if auth['info']
-        user.name = auth['info']['name'] || ""
-        user.email = auth['info']['email'] || ""
-        user.password =  auth['credentials']['token']|| ""
-      end
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(name:auth.extra.raw_info.name,
+                           provider:auth.provider,
+                           uid:auth.uid,
+                           email:auth.info.email,
+                           password:Devise.friendly_token[0,20]
+                           )
     end
+    user
   end
 
 private
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '169919403205681', '1645b0e34cb329d8386d03a5caa7e644',
-           :scope => 'email, read_stream, read_friendlists, offline_access', :display => 'popup'
-end
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, '169919403205681', '1645b0e34cb329d8386d03a5caa7e644',
+#            :scope => 'email, read_stream, read_friendlists, offline_access', :display => 'popup'
+# end

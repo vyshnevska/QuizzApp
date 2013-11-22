ced2edb5907274e965d846b913111cfaf38044d1
Nina Vyshnevska <vyshnevska.n@gmail.com>
2013-11-22 21:11:05 +0200

    Fixed route for fb.

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,9 @@ QuizzApp::Application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  devise_for :users
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  # devise_for :users
 
   root :to => "games#welcome"
 
